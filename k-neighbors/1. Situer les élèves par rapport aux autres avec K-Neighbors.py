f7bd3cef145f6a66a5a1c7fc795158e42a5e9290
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-01-14 19:10:56 +0100

    1. Situer les élèves par rapport aux autres avec K-Neighbors

---
+++ k-neighbors/1. Situer les élèves par rapport aux autres avec K-Neighbors.py
@@ -0,0 +1,12 @@
+""" 
+Dans un premier temps :
+
+Utiliser un  K-neighbors pour regrouper les élèves par notes .
+
+Dans un dexuième temps :
+
+Trouver un moyen de répartir équitablement les élvèves par callse ou sous contraintes de notes , peut être avec bin packing ??
+
+A FAIRE 
+
+"""
