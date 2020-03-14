5f675d2a4f28cbec8e1f280ff0c3da32110fcb5d
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-03-14 11:41:51 +0100

    10. Exponentielle - Quelle est la probabilité quun objet casse avant la 100 eme heure.py

---
+++ lois de probabilités/cas reels d'utilisation/10. Exponentielle - Quelle est la probabilité quun objet casse avant la 100 eme heure.py
@@ -0,0 +1,48 @@
+""" 
+
+    Loi exponentielle de paramêtre lambda.
+
+   Source :
+   YVAN MONKA 
+   https://www.youtube.com/watch?v=ZS_sW8yq-94&list=LLbfwcLDelivztPdafMQ42WQ&index=5&t=0s
+
+   STANFORD
+   http://web.stanford.edu/class/archive/cs/cs109/cs109.1192/handouts/pythonForProbability.html
+
+
+ """
+
+ # Problème : 
+"""  
+La durée de vie, en heures, d'un composant électronique est une variable aléatoire 
+qui suit une loi exponentielle de paramètre Lambda =  0.0035
+
+1. Quelle est la probabilité qu'il casse avant 100 heures ?
+
+Formule : P(X < 100) = 1 - e ** -0.0035 * 100 = 0.3 = 30%
+
+2. Quelle est sa durée moyenne de vie ?
+
+Formule : 1 \ 0.0035 = 286 heures
+
+"""
+
+ # Import des librairies
+from scipy import stats
+import math   # This will import math module
+
+# CALCUL PRIMAIRE AVEC LA LIB MATH
+
+lambdaCalcul = - 0.0035 * 100
+MaProbabiliteExponentielle = 1 - math.exp(lambdaCalcul)
+
+print("J'ai calculé avec un calcul primaire ma probabilité ",MaProbabiliteExponentielle * 100 , "%");
+
+
+# ESSAI DE CALCUL AVEC SCIPY = ECHEC , B.cdf est faux.
+#  This example declares B∼Exp(λ=0.0035)
+B = stats.expon(0.0035)   # Declare B to be a normal random variable
+print(B.pdf(1))       # f(1), the probability density at 1
+print(B.cdf(100))       # F(2) which is also P(B < 100)
+print(B.rvs())        # Get a random sample from B
+
