9448b601b61ed1b63711f87ea3eaf2de64019364
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-06-05 09:27:32 +0200

    Suppress

--- divers/test-4-matplot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.linspace(-2, 2, 500)
-plt.plot(x, np.around(x,0), label="around(x,0)")
-plt.plot(x, np.trunc(x), label="trunc(x)")
-plt.legend()
-
-plt.show()
