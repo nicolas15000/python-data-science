4751a1328559cee6b3853051a4a0dcea9e2e2dbf
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-06-05 08:07:57 +0200

    Mise en place du répertoire classifications

---
+++ classification/1. classifier des fromages grâce à leurs caracteristiques.py
@@ -0,0 +1,17 @@
+""" 
+Classification de fromages
+
+Cours :
+Institut de Mathématiques de Bordeaux - Université de Bordeaux
+http://www.math.u-bordeaux.fr/~mchave100p/teaching/
+
+TP3 [sujet][fromage.txt][correction algos à la main]
+
+Ce tutorial :  
+Classification automatique sous python de Ricco Rakotomalala
+http://eric.univ-lyon2.fr/~ricco/cours/
+
+"""
+
+
+
