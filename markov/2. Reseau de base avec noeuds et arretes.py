7d48acd94119ed103d3d6a632eb13ead7b26131d
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-06-08 21:41:15 +0200

    indentation

--- markov/2. Reseau de base avec noeuds et arretes.py
+++ markov/2. Reseau de base avec noeuds et arretes.py
@@ -19,15 +19,20 @@ val_map = {'A': 1.0,
                               'H': 0.0}
 
 values = [val_map.get(node, 0.45) for node in G.nodes()]
+
 edge_labels=dict([((u,v,),d['weight'])
                  for u,v,d in G.edges(data=True)])
+
 red_edges = [('C','D'),('D','A')]
+
 edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
 
 pos=nx.spring_layout(G)
 
 node_labels = {node:node for node in G.nodes()}; nx.draw_networkx_labels(G, pos, labels=node_labels)
+
 nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+
 nx.draw(G,pos, node_color = values, node_size=1500,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
 
 plt.show()
