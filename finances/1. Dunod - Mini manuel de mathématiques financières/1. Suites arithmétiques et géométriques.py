cda97d4d9730ad5ff7030881b259f71c65b7a2a1
NIcolas <nicolas.huleux@mail.com>
2020-10-04 18:15:55 +0200

    Commit 1

--- finances/1. Dunod - Mini manuel de mathématiques financières/1. Suites arithmétiques et géométriques.py
+++ finances/1. Dunod - Mini manuel de mathématiques financières/1. Suites arithmétiques et géométriques.py
@@ -4,16 +4,23 @@ Suites arithmétiques et géométriques en python pour la finance :
 Sources : 
 
 1. Dunod - Mini manuel de mathématiques financières
+2. Dunod - Méthodes mathématiques pourl'informatique pour le BTS SIO
 2. Yvan Monka - SUITES : Calculer la somme des termes d'une suite (ALGORITHME) - Tutoriel PYTHON """
 
-""" 
 
+
+""" 
+PARTIE 1 :
 Suite arithmétique 
 La raison d'une suite arithmétique est r.
 U0 est le premier terme, U1 le second ,U2 le troisième et ainsi de suite : Un
 
 Définition globale:
 Un + 1 = Un + r 
+
+
+Les suites arithmétiques se retrouvent lorsque il y a un ajout ou un retrait d'une même somme à la meme période .
+Loyer + 400 par mois, Retirer 100E de son compte tous les ans, intérêts simples , production augemente de 100 par jours.
 
 """
 # -------------------------------------------------------------------------------
@@ -29,9 +36,9 @@ n = 5        # on veut calculer le terme de rang 5
 raison = 2   # on ajoute 2 à chaque fois.
 
 # Un = U0 + n * r
-u5 = 1 + n * raison
+u5 = u0 + n * raison
 print(u5)
-# ça nous donne 11 pour le terme de rang 5
+# ça nous donne la valeur de 11 pour le terme de rang 5
 
 # -------------------------------------------------------------------------------
 # 2. Calculer la somme des termes d'une suite arithmétique :
@@ -46,15 +53,34 @@ def somme(n):
            
     return(s)
 
-print(somme(2)) # On itère jusqu'au terme 2
+print(somme(2)) # On itère jusqu'au terme u3 ( La boucle for commence à 0, donc ca compte 3 termes)
 # La somme est de u0+ u1 +u2 = 1 + 3 + 5 = 9
 
-""" Ou avec cette formule  Dunod - Mini manuel de mathématiques financières:
-S = ((1er terme + dernier terme) * Nb de terme) / 2 
+""" 
+Seconde solution : 
+Avec cette formule  Dunod - Mini manuel de mathématiques financières:
+
+Ma_somme = ((1er terme + dernier terme) * Nb de termes) / 2 
 """
 
 Ma_somme = ((1 + 5) * 3) / 2
-print(Ma_somme)
+print(Ma_somme) #  La somme est 9
+
+""" 
+PARTIE 2 :
+Suite géométriques 
+La raison d'une suite géométrique est q.
+U0 est le premier terme, U1 le second ,U2 le troisième et ainsi de suite : Un
+
+Définition globale:
+Un + 1 = q * Un
+
+"""
+# -------------------------------------------------------------------------------
+# 1. Calculer un terme d'une suite géométrique si on a u0 ( Le premier terme):
+# Un = q^n * U0
+# -------------------------------------------------------------------------------
+
 
 
 
