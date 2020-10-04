9a0d1aa81a1cc139961a34f0c33c71eaf0765a33
NIcolas <nicolas.huleux@mail.com>
2020-10-04 19:22:13 +0200

    Les intérêts simples et l'escompte en python

---
+++ finances/1. Dunod - Mini manuel de mathématiques financières/2. Les intérêts imples et l'escompte.py
@@ -0,0 +1,9 @@
+""" 
+Les intérêts simples et l'escompte :
+
+Sources : 
+
+1. Dunod - Mini manuel de mathématiques financières
+2. Wikibooks : https://fr.wikibooks.org/wiki/Math%C3%A9matiques_avec_Python_et_Ruby/Suites_en_Python
+
+"""
