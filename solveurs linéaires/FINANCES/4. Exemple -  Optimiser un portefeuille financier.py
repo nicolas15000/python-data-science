fb4ec42ed408a678872cda5c2280305f80814386
NIcolas <nicolas.huleux@mail.com>
2020-10-04 19:31:19 +0200

    Ajout du nouveau problème linéaire de portefeuille financier.

---
+++ solveurs linéaires/FINANCES/4. Exemple -  Optimiser un portefeuille financier.py
@@ -0,0 +1,15 @@
+""" 
+** Problème d'optimisation financière 4 : un portefeuille.
+
+L'optimisation du portefeuille est le processus de sélection du meilleur portefeuille (répartition des actifs), 
+parmi l'ensemble de tous les portefeuilles considérés, selon un objectif.
+L'objectif maximise généralement les facteurs tels que le rendement attendu 
+ et minimise les coûts tels que le risque financier.
+-Wikipédia
+
+** Source : 
+https://medium.com/@randerson112358/python-for-finance-portfolio-optimization-66882498847
+
+
+
+ """
