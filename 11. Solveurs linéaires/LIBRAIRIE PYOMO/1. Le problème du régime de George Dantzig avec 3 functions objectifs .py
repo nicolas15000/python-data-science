80743312901bf92c8a6ad1043b4017c8412c5634
NIcolas <nicolas.huleux@mail.com>
2021-01-04 17:48:06 +0100

    Pyomo ( fonctionne mal , à revoir)

--- 11. Solveurs linéaires/LIBRAIRIE PYOMO/1. Le problème du régime de George Dantzig avec 3 functions objectifs .py
+++ 11. Solveurs linéaires/LIBRAIRIE PYOMO/1. Le problème du régime de George Dantzig avec 3 functions objectifs .py
@@ -88,3 +88,49 @@ model.V    = Param(model.F, within=PositiveReals)
 # Volume maximum d'aliments consommés
 model.Vmax = Param(within=PositiveReals)
 
+
+
+""" L'option within est utilisée dans ces déclarations de paramètres pour définir 
+les propriétés attendues des paramètres. Ces informations sont utilisées pour 
+effectuer des vérifications d'erreurs sur les données utilisées pour initialiser 
+les composants de paramètre.
+
+Le composant Var permet de définir les variables de décision: """
+
+# Nombre de portions consommées de chaque aliment
+model.x = Var(model.F, within=NonNegativeIntegers)
+
+""" L'option within est utilisée pour restreindre le domaine des variables de décision aux réels non négatifs. 
+Cela élimine le besoin de contraintes liées explicites pour les variables.
+
+Le composant Objectif est utilisé pour définir l'objectif de coût. 
+Ce composant utilise une fonction de règle pour construire l'expression objective: """
+
+
+
+# Minimiser le coût des aliments consommés, on voit que c'est une function
+def cost_rule(model):
+    return sum(model.c[i]*model.x[i] for i in model.F)
+model.cost = Objective(rule=cost_rule)
+
+ #De même, les fonctions de règle sont utilisées pour définir des expressions de contrainte dans le composant Constraint:
+
+# Limiter la consommation de nutriments pour chaque nutriment
+def nutrient_rule(model, j):
+    value = sum(model.a[i,j]*model.x[i] for i in model.F)
+    return model.Nmin[j] <= value <= model.Nmax[j]
+model.nutrient_limit = Constraint(model.N, rule=nutrient_rule)
+
+# Limitez le volume de nourriture consommée
+def volume_rule(model):
+    return sum(model.V[i]*model.x[i] for i in model.F) <= model.Vmax
+model.volume = Constraint(rule=volume_rule)
+
+""" 
+Données du modèle
+
+Puisqu'il s'agit d'un modèle Pyomo abstrait, les valeurs d'ensemble et de paramètre doivent être fournies pour initialiser le modèle. Le fichier de commande de données suivant fournit un ensemble de données synthétiques:
+
+ """
+
+ 
